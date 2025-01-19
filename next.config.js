3ed9d15bd9f655455ed077870ada358783d0cb32
Friedrich Chen <friedrichchen@Inuyasha.local>
2025-01-19 18:17:42 +0800

    feat: update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 const { withContentlayer } = require("next-contentlayer");
+const isGithubPages = process.env.NODE_ENV === "production";
+const repositoryName = "jalever.github.io";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: "export",
-  distDir: "docs",
+  distDir: "docs", // 输出到 docs 目录
+  assetPrefix: isGithubPages ? `/${repositoryName}/` : "", // 设置静态资源前缀
+  basePath: isGithubPages ? `/${repositoryName}` : "", // 设置基础路径
 };
 
 module.exports = withContentlayer(nextConfig);
